a69b490e77a723e39e72d3662cd18c6378b81e50
matt384 <108557364+matt384@users.noreply.github.com>
2022-07-19 14:25:20 -0700

    removed MI references (#3)
    * specify protocol in the .env
    
    * removed MI references to gifted chat
    
    Co-authored-by: matt384 <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>

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const config = {
-  ROOM_SERVER: "https://" + process.env.REACT_APP_ROOM_SERVER + "/api/room/",
-  ROOM_API: "https://" + process.env.REACT_APP_ROOM_SERVER + "/api",
-  ROOM_SERVER_WS: "wss://" + process.env.REACT_APP_ROOM_SERVER + "/api/room/",
-  STORAGE_SERVER: "https://" + process.env.REACT_APP_STORAGE_SERVER + "/api/v1"
+  ROOM_SERVER: process.env.REACT_APP_ROOM_SERVER + "/api/room/",
+  ROOM_API: process.env.REACT_APP_ROOM_SERVER + "/api",
+  ROOM_SERVER_WS: process.env.REACT_APP_ROOM_SERVER_WS + "/api/room/",
+  STORAGE_SERVER: process.env.REACT_APP_STORAGE_SERVER + "/api/v1"
 }
 
 export default config;
