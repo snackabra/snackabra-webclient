fb78b0ceb30da39485504a6efe16f6f27065f349
matt384 <108557364+matt384@users.noreply.github.com>
2022-12-05 16:29:07 -0800

    create room fix

--- src/components/Rooms/CreateRoom.js
+++ src/components/Rooms/CreateRoom.js
@@ -5,7 +5,8 @@ import { StyledButton } from "../../styles/Buttons";
 import CircularProgress from '@mui/material/CircularProgress';
 import { useState, useContext } from "react"
 import NotificationContext from "../../contexts/NotificationContext";
-import {observer} from "mobx-react"
+import FirstVisitDialog from "../Modals/FirstVisitDialog";
+import { observer } from "mobx-react"
 import { SnackabraContext } from "mobx-snackabra-store";
 
 
@@ -13,17 +14,22 @@ const CreateRoom = observer((props) => {
   const sbContext = React.useContext(SnackabraContext);
   const Notifications = useContext(NotificationContext)
   const [secret, setSecret] = useState('');
+  const [roomId, setRoomId] = useState('');
   const [creating, setCreating] = useState(false);
+  const [openFirstVisit, setOpenFirstVisit] = useState(false);
 
   const success = (roomId) => {
+    if (typeof props?.onClose === 'function') {
+      props.onClose()
+    }
     Notifications.setMessage('Room Created!');
     Notifications.setSeverity('success');
     Notifications.setOpen(true)
     setCreating(false)
-    setTimeout(()=>{
-      window.location.href = window.location.origin+`/${roomId}`
+    setTimeout(() => {
+      window.location.href = window.location.origin + `/${roomId}`
     }, 750)
-    
+
   }
 
   const error = () => {
@@ -36,23 +42,35 @@ const CreateRoom = observer((props) => {
   const createRoom = async () => {
     try {
       setCreating(true)
-      const roomId = await sbContext.createRoom(secret)
-      if (typeof props?.onClose === 'function') {
-        props.onClose()
-      }
-      success(roomId);
+      sbContext.createRoom(secret).then((channel) => {
+        setRoomId(channel)
+        setOpenFirstVisit(true)
+      })
     } catch (e) {
       console.error(e)
       error()
     }
   }
 
+  const saveUsername = (newUsername) => {
+    return new Promise((resolve) => {
+      const _id = sbContext.user._id;
+      sbContext.username = newUsername;
+      const contacts = sbContext.contacts
+      const user_pubKey = _id;
+      contacts[user_pubKey.x + ' ' + user_pubKey.y] = newUsername;
+      sbContext.contacts = contacts;
+      resolve(true)
+    })
+
+  }
+
   return (
     <Grid spacing={2}
-          container
-          direction="row"
-          justifyContent="flex-start"
-          alignItems="flex-start">
+      container
+      direction="row"
+      justifyContent="flex-start"
+      alignItems="flex-start">
 
       <Grid xs={12} item>
         <TextField
@@ -65,19 +83,25 @@ const CreateRoom = observer((props) => {
         />
       </Grid>
       <Grid xs={12} item>
-      {creating ?
-            <StyledButton disabled variant={'outlined'}>
-              <CircularProgress color={"success"} size={20} />
-              &nbsp;Creating
-            </StyledButton>
+        {creating ?
+          <StyledButton disabled variant={'outlined'}>
+            <CircularProgress color={"success"} size={20} />
+            &nbsp;Creating
+          </StyledButton>
 
-            :
-            <StyledButton variant="contained" onClick={createRoom}><Trans id='new room header'>Create New
+          :
+          <StyledButton variant="contained" onClick={createRoom}><Trans id='new room header'>Create New
             Room</Trans></StyledButton>
-          }
+        }
 
       </Grid>
+      <FirstVisitDialog open={openFirstVisit} sbContext={sbContext} onClose={(username) => {
+        saveUsername(username).then(()=>{
+          setOpenFirstVisit(false)
+          success(roomId);
+        })
 
+      }} roomId={roomId} />
     </Grid>
   )
 })
