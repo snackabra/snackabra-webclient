45885e7d3d1240fca8e7aa85d54f76609a03f265
matt384 <108557364+matt384@users.noreply.github.com>
2022-12-10 19:34:34 -0800

    fix for SB384-51

--- src/utils/ImageProcessor.js
+++ src/utils/ImageProcessor.js
@@ -58,48 +58,46 @@ export async function _restrictPhoto(maxSize, _c, _b1, scale) {
   console.log(`Reduce size by scaling canvas - start size is W ${_c.width} x H ${_c.height}`)
   // compression wasn't enough, so let's resize until we're getting close
 
-  // let _old_size;
-  // let _old_c;
+  let _old_size;
+  let _old_c;
 
   while (_size > maxSize) {
-    // _old_c = _c;
+    _old_c = _c;
     _c = scaleCanvas(_c, scale);
     _b1 = await new Promise((resolve) => {
       // TODO: lint reports this as unsafe use of reference to _c
       _c.toBlob(resolve, imageType, qualityArgument);
     });
-    // _old_size = _size;
+    _old_size = _size;
     _size = _b1.size;
     // workingDots();
     const t3 = new Date().getTime();
     console.log(`... reduced to W ${_c.width} x H ${_c.height} (to size ${_size}) ... total time ${t3 - t2} milliseconds`);
   }
 
-  // // we assume that within this width interval, storage is roughly prop to area,
-  // // with a little tuning downwards
-  // let _ratio = (maxSize / _old_size) * 0.3; // overshoot a bit
-  // let _maxIteration = 3;  // to be safe
-  // console.log("_old_c is:")
-  // console.log(_old_c);
-  // console.log(`... stepping back up to W ${_old_c.width} x H ${_old_c.height} and will then try scale ${_ratio.toFixed(4)}`);
-  // let _final_c;
-  // const t4 = new Date().getTime();
-  // let goodRatio = false
-  // do {
-  //   // TODO: lint reports this as unsafe reference to _final_c
-  //   _final_c = scaleCanvas(_old_c, Math.sqrt(_ratio) * 0.3); // always overshoot
-  //   _b1 = await new Promise((resolve) => {
-  //     _final_c.toBlob(resolve, imageType, qualityArgument);
-  //     console.log(`(generating blob of requested type ${imageType})`);
-  //   });
-  //   // workingDots();
-  //   console.log(`... fine-tuning to W ${_final_c.width} x H ${_final_c.height} (size ${_b1.size})`);
-  //   _ratio *= (maxSize / _b1.size);
-  //   const t5 = new Date().getTime();
-  //   console.log(`... resulting _ratio is ${_ratio} ... total time here ${t5 - t4} milliseconds`);
-  //   console.log(` ... we're within ${(Math.abs(_b1.size - maxSize) / maxSize)} of cap (${maxSize})`);
-  //   goodRatio = ((Math.abs(_b1.size - maxSize) / maxSize) < .9 && (Math.abs(_b1.size - maxSize) / maxSize) > .8)
-  // } while ((((_b1.size >= maxSize) || ((Math.abs(_b1.size - maxSize) / maxSize) > 0.10)) && (--_maxIteration > 0)) && !goodRatio);  // we're pretty tolerant here
+  // we assume that within this width interval, storage is roughly prop to area,
+  // with a little tuning downwards
+  let _ratio = (maxSize / _old_size) * 0.95; // overshoot a bit
+  let _maxIteration = 12;  // to be safe
+  console.log("_old_c is:")
+  console.log(_old_c);
+  console.log(`... stepping back up to W ${_old_c.width} x H ${_old_c.height} and will then try scale ${_ratio.toFixed(4)}`);
+  let _final_c;
+  const t4 = new Date().getTime();
+  do {
+    // TODO: lint reports this as unsafe reference to _final_c
+    _final_c = scaleCanvas(_old_c, Math.sqrt(_ratio) * 0.95); // always overshoot
+    _b1 = await new Promise((resolve) => {
+      _final_c.toBlob(resolve, imageType, qualityArgument);
+      console.log(`(generating blob of requested type ${imageType})`);
+    });
+    // workingDots();
+    console.log(`... fine-tuning to W ${_final_c.width} x H ${_final_c.height} (size ${_b1.size})`);
+    _ratio *= (maxSize / _b1.size);
+    const t5 = new Date().getTime();
+    console.log(`... resulting _ratio is ${_ratio} ... total time here ${t5 - t4} milliseconds`);
+    console.log(` ... we're within ${(Math.abs(_b1.size - maxSize) / maxSize)} of cap (${maxSize})`);
+  } while ((((_b1.size >= maxSize) || ((Math.abs(_b1.size - maxSize) / maxSize) > 0.10)) && (--_maxIteration > 0)));  // we're pretty tolerant here
 
   return _b1;
 }
@@ -114,7 +112,7 @@ export async function restrictPhoto(sbImage, maxSize, type) {
 
   switch (type) {
     case 'thumbnail':
-      scale = .15;
+      scale = sbImage.size > 15560 * 1024 ? .15 : .8;
       console.log(type, scale)
       break;
     case 'preview':
