227d10e7af98e9eea237c4330edd4e6f36bb9de8
matt384 <108557364+matt384@users.noreply.github.com>
2022-12-20 10:33:45 -0800

    Fix for message area

--- src/components/Chat/RenderComposer.js
+++ src/components/Chat/RenderComposer.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import TextField from '@mui/material/TextField';
 
 // style="--placeholderTextColor:#b2b2b2; height: 34px; outline-width: 0px; outline-color: transparent; outline-offset: 0px;"
 
@@ -12,10 +13,8 @@ function RenderComposer(props) {
   }, [])
 
   const handleSend = () => {
-    setTimeout(()=>{
       setText('')
       props.onTextChanged('')
-    },250)
   }
 
   React.useEffect(() => {
@@ -29,6 +28,8 @@ function RenderComposer(props) {
   const checkForSend = (e) => {
     if (e.keyCode === 13 && !e.ctrlKey && !e.shiftKey) {
       document.getElementById('send-button').click()
+      const input = document.getElementById('send-sb_render_composer_textarea');
+      input.value = ""
       handleSend();
     }
   }
@@ -39,22 +40,63 @@ function RenderComposer(props) {
   }
 
   return (
-    <textarea placeholder="Type a message..." autoCapitalize="sentences" autoComplete="on" autoCorrect="on" dir="auto"
-              value={text}
-              rows="1" spellCheck="true" aria-label="Type a message..."
-              className="textinput-composer"
-              data-testid="Type a message..."
-              onKeyUp={checkForSend}
-              onChange={handlChange}
-              readOnly={filesAttached}
-              style={{
-                '--placeholderTextColor': '#b2b2b2',
-                height: '34px',
-                outlineWidth: '0px',
-                outlineColor: 'transparent',
-                outlineOffset: '0px'
-              }}
+    <TextField
+      id="sb_render_composer_textarea"
+      label=""
+      value={text}
+      placeholder="Type a message..."
+      className="textinput-composer"
+      multiline
+      onKeyUp={checkForSend}
+      onChange={handlChange}
+      readOnly={filesAttached}
+      variant={'standard'}
+      InputProps={{
+        disableUnderline: true
+      }}
+      style={{
+        flexGrow: 1,
+        padding: 8
+      }}
     />
+    // <textarea placeholder="Type a message..." autoCapitalize="sentences" autoComplete="on" autoCorrect="on" dir="auto"
+    //   value={text}
+    //   id="sb_render_composer_textarea"
+    //   rows="1" spellCheck="true" aria-label="Type a message..."
+    //   className="MuiInput-root"
+    //   data-testid="Type a message..."
+    //   onKeyUp={checkForSend}
+    //   onChange={handlChange}
+    //   readOnly={filesAttached}
+    //   style={{
+    //     '--placeholderTextColor': '#b2b2b2',
+    //     height: 34,
+    //     outlineWidth: 'opx !important',
+    //     outlineColor: 'transparent !important',
+    //     outlineStyle: 'none  !important',
+    //     // outlineWidth: '0px  !important',
+    //     // outlineColor: 'transparent !important',
+    //     outlineOffset: '0px  !important',
+    //     // outlineStyle: 'none !important',
+    //     font: 'inherit',
+    //     letterSspacing: 'inherit',
+    //     color: 'currentColor',
+    //     padding: 0,
+    //     border: '0',
+    //     boxSizing: 'content-box',
+    //     background: 'none',
+    //     // height: '1.4375em',
+    //     margin: '0',
+    //     '-webkit-tap-highlight-color': 'transparent',
+    //     display: 'block',
+    //     minWidth: '0',
+    //     width: '100%',
+    //     webkitAnimationName: 'mui-auto-fill-cancel',
+    //     animationName: 'mui-auto-fill-cancel',
+    //     '-webkit-animation-duration': '10ms',
+    //     animationDuration: '10ms',
+    //   }}
+    // />
   )
 }
 
